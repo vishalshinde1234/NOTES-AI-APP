2e412b261e2f97f7a02232dcbf064dd196ea55ff
vishalshinde1234 <a021vishalshinde@gmail.com>
2026-03-29 12:13:44 +0530

    removed auth middleware properly

--- backend/src/routes/aiRoutes.js
+++ backend/src/routes/aiRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { generateNote, summarizeNote, improveNote, makeBullets } = require('../controllers/aiController');
-const { protect } = require('../middleware/auth');
+
 
 
 router.post('/generate', generateNote);
